a9a57c34abcd5092d6da6637f2957e41769ed579
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-03 17:19:02 +0100

    Main completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,12 +32,49 @@
 
     {{-- main --}}
     <main>
+      {{-- LUNGHE --}}
       <section class="container">
         <h2>le lunghe</h2>
 
         {{-- contenitore delle card --}}
         <div class="cards_rule">
           @foreach ($lunghe as $pasta)
+          <div class="card">
+            <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+            <div class="card_text">
+              <a href="#">{{ $pasta["titolo"] }}</a>
+            </div>
+          </div>
+          @endforeach
+        </div>
+
+      </section>
+
+      {{-- CORTE --}}
+      <section class="container">
+        <h2>le corte</h2>
+
+        {{-- contenitore delle card --}}
+        <div class="cards_rule">
+          @foreach ($corte as $pasta)
+          <div class="card">
+            <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
+            <div class="card_text">
+              <a href="#">{{ $pasta["titolo"] }}</a>
+            </div>
+          </div>
+          @endforeach
+        </div>
+
+      </section>
+
+      {{-- CORTISSIME --}}
+      <section class="container">
+        <h2>le cortissime</h2>
+
+        {{-- contenitore delle card --}}
+        <div class="cards_rule">
+          @foreach ($cortissime as $pasta)
           <div class="card">
             <img src="{{ $pasta["src"] }}" alt="{{ $pasta["titolo"] }}">
             <div class="card_text">
