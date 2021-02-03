e62ff588fcfed72cf7137e29f7572e3eb05e5f22
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-03 18:31:11 +0100

    Correzioni
    1 - Aggiunta sfondo al main della pagina home
    2 - Contenuto di base alla pagina news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.main-layout')
 
 @section('content')
-  <h1>Latest News</h1>
+<main>
+  <div class="container">
+    <h1>Latest News</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis autem dolorem deleniti cupiditate, tenetur voluptatem alias esse officiis quaerat, velit eveniet harum unde facere, est blanditiis ut error commodi dolorum.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis autem dolorem deleniti cupiditate, tenetur voluptatem alias esse officiis quaerat, velit eveniet harum unde facere, est blanditiis ut error commodi dolorum.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis autem dolorem deleniti cupiditate, tenetur voluptatem alias esse officiis quaerat, velit eveniet harum unde facere, est blanditiis ut error commodi dolorum.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis autem dolorem deleniti cupiditate, tenetur voluptatem alias esse officiis quaerat, velit eveniet harum unde facere, est blanditiis ut error commodi dolorum.</p>
+  </div>
+</main>
 @endsection
