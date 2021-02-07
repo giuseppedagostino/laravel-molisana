be518e25af2f6ff2aedd12c1e220f9a66a8939d2
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-07 18:44:49 +0100

    Prima sistemazione css

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.main-layout')
 
 @section('content')
+
 <div class="container">
-  <h2>sezione prodotto {{ $product["titolo"] }}</h2>
+  <div class="product_wrapper">
+    <h2>{{ $product["titolo"] }}</h2>
+    <h3>Tipologia: {{ $product["tipo"] }}</h3>
+    <h3>Peso: {{ $product["peso"] }}</h3>
+    <h4>{{ $product["descrizione"] }}</h4>
+  </div>
 </div>
+
 @endsection
